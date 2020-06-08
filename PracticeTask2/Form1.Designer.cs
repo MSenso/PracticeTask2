7d4cc788a0ffa9b46f6234ea22d3cb44a4591de4
skjfdnv <skjfdnv@yandex.ru>
2020-06-08 14:50:04 +0500

    Upd

--- PracticeTask2/Form1.Designer.cs
+++ PracticeTask2/Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(245)))), ((int)(((byte)(248)))));
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(78, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вывод статистики";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe Print", 11F);
+            this.textBox1.Location = new System.Drawing.Point(12, 86);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(394, 260);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::PracticeTask2.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(418, 373);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Статистика по пакетам";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- PracticeTask2/Form1.cs
+++ PracticeTask2/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,51 @@ namespace PracticeTask2
 {
     public partial class Form1 : Form
     {
+        static string ping_name;
         public Form1()
         {
             InitializeComponent();
+        }
+        void Run()
+        {
+            string[] data = File.ReadAllLines("INPUT.TXT");
+            string ping_name = data[0].Split(' ')[1];
+            string[] packages = new string[data.Length - 1];
+            int count_loss = 0;
+            int average = 0, min = (int)Math.Pow(10, 4), max = 0;
+            for (int i = 0; i < packages.Length; i++)
+            {
+                if (data[i + 1] == "Time out")
+                {
+                    count_loss++;
+                    packages[i] = data[i + 1];
+                }
+                else
+                {
+                    packages[i] = data[i + 1].Split(' ')[3].Split('=')[1];
+                    min = Math.Min(min, int.Parse(packages[i]));
+                    max = Math.Max(max, int.Parse(packages[i]));
+                    average += int.Parse(packages[i]);
+                }
+            }
+            double loss = (double)count_loss / packages.Length;
+            loss = Math.Round(loss * 100, 2);
+            string output = $"Ping statistics for {ping_name}: \n" +
+                $"Packets: Sent = {packages.Length} Received = {packages.Length - count_loss} Lost = {count_loss} ({loss}% loss)";
+            if (count_loss != packages.Length)
+            {
+                average = (int)Math.Round((double)average / (packages.Length - count_loss), MidpointRounding.AwayFromZero);
+                output += $"\n" +
+                $"Approximate round trip times: \n" +
+                $"Minimum = {min} Maximum = {max} Average = {average}";
+            }
+            output = output.Replace("\n", Environment.NewLine);
+            File.WriteAllText("OUTPUT.TXT", output);
+            textBox1.Text = output;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Run();
         }
     }
 }
